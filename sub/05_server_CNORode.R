b1049dabd79f635005d5913bc03dc11ebaafc48c
gabora <gabor.attila87@gmail.com>
2019-04-21 13:07:51 +0200

    CNORode code organised as in cellnoptr tab CNORode extended with user inputs optimisation changed to eSS

--- sub/05_server_CNORode.R
+++ sub/05_server_CNORode.R
@@ -1,42 +1,75 @@
-# --- Output CNORode --- # 
+# ---  CNORode reactive data--- # 
 
-preprocessed_model_CNORode <- NULL
-res_CNORode <- NULL
+
+preprocessed_model_CNORode <- reactive({
+  
+  req(SIF_model(),CNO())
+  print(input$maxTime_input)
+  prep_model <- preprocessing(model = SIF_model(), data = CNO(),cutNONC = T,
+                              compression = input$compression_CNORode,
+                              expansion = FALSE)
+  return(prep_model)
+  
+})
+
+
+# --- Output CNORode --- # 
+# res_CNORode: stores the model, data and optim results in a synchronised way.
+# OptimalFit and optimNetwork plots are dependent on this data
+res_CNORode = reactiveValues(pkn_model = NULL, 
+                             cno_data = NULL,
+                             optim_results = NULL,
+                             transfer_function = NULL)
+
+
+# run optimisation upon button pressed:
+observeEvent(input$run_CNORode,{
+  
+  req(preprocessed_model_CNORode(),CNO())
+  
+  # Initialise parameters
+  initial_pars=createLBodeContPars(preprocessed_model_CNORode(), 
+                                   LB_n = input$n_range_slider[[1]], UB_n = input$n_range_slider[[2]],
+                                   LB_k = input$k_range_slider[[1]], UB_k = input$k_range_slider[[2]],
+                                   LB_tau = input$tau_range_slider[[1]], UB_tau = input$tau_range_slider[[2]],
+                                   random = TRUE)
+  
+  opt_pars <- parEstimationLBodeSSm(cnolist = CNO(),
+                        model = preprocessed_model_CNORode(),
+                        ode_parameters = initial_pars,
+                        maxeval = input$maxEval_ode_input,
+                        maxtime = input$maxTime_ode_input,
+                        transfer_function = input$transferFunction_input)
+  
+  res_CNORode$pkn_model <- preprocessed_model_CNORode()
+  res_CNORode$cno_data <- CNO()
+  res_CNORode$optim_results <- opt_pars
+  res_CNORode$transfer_function <- input$transferFunction_input
+  
+},ignoreInit = TRUE)
 
 # --- PlotModel CNORode --- #
 
+output$PlotPrepModel_CNORode <-  renderPlot({
+  
+  req(preprocessed_model_CNORode())
+  
+  plotModel(model = preprocessed_model_CNORode(),CNOlist = CNO())
+  
+})
+
+# --- PlotFit CNORode --- #
+
+# triggered when res_CNORode gets initialised/updated
 output$PlotFit_CNORode <- renderPlot({
   
-  if (input$run_CNORode) {
-    
-    # Pre-processing CNORode (set gate expansion to False by default[?])
-    # preprocessed_model_CNORode <<- preprocessing(model = SIF_model, data = cno,cutNONC = T,
-    #                                              compression = input$compression_CNORode,
-    #                                              expansion = F )
-    preprocessed_model_CNORode <<- preprocessing(model = SIF_model(), data = CNO(),cutNONC = T,
-                                                 compression = input$compression_CNORode,
-                                                 expansion = F )
-    
-    # Initialise parameters
-    initial_pars=createLBodeContPars(preprocessed_model_CNORode, LB_n = 1, LB_k = 0.1,
-                                     LB_tau = 0.01, UB_n = 5, UB_k = 0.9, UB_tau = 10, random = TRUE)
-    paramsGA = defaultParametersGA()
-    paramsGA$maxStepSize = 1
-    paramsGA$popSize = 50
-    paramsGA$iter = 100
-    paramsGA$transfer_function = 2
-    
-    # Optimisation
-    # opt_pars=parEstimationLBode(cno,preprocessed_model_CNORode,ode_parameters=initial_pars,
-    #                             paramsGA=paramsGA)
-    opt_pars=parEstimationLBode(CNO(),preprocessed_model_CNORode,ode_parameters=initial_pars,
-                                paramsGA=paramsGA)
-    
-    # Plotting
-    # plotLBodeFitness(cno, preprocessed_model_CNORode,ode_parameters=opt_pars)
-    plotLBodeFitness(CNO(), preprocessed_model_CNORode,ode_parameters=opt_pars)
-    
-  }
+  if(is.null(res_CNORode$optim_results)) return(NULL)
+  plotLBodeFitness(res_CNORode$cno_data,
+                   res_CNORode$pkn_model,
+                   ode_parameters=res_CNORode$optim_results,
+                   transfer_function = res_CNORode$transfer_function)
+  
 })
 
 # --- End of the script --- #
+
--- sub/05_ui_CNORode.R
+++ sub/05_ui_CNORode.R
@@ -9,10 +9,27 @@ tabPanel(
       helpText("Preprocessing CNORode"),
       checkboxInput("compression_CNORode", "Compression CNORode", FALSE),
       # checkboxInput("expansion_CNORode", "OR-gate expansion", FALSE),
-      actionButton("run_CNORode", label="Run CNORode")),
+      helpText("Interaction types:"),
+      radioButtons("transferFunction_input", "Transfer function type:",
+                   c("Hill" = "2",
+                     "Normalised Hill" = "3",
+                     "Inv-norm-Hill" = "4"),selected = "4"),
+      actionButton("run_CNORode", label="Run CNORode"),
+      helpText("Optimisation bounds:"),
+      sliderInput("k_range_slider", "Bounds for parameter k:",
+                  min = 0, max = 10, value = c(1,5),step = 0.1),
+      sliderInput("n_range_slider", "Bounds for parameter n:",
+                  min = 1, max = 10, value = c(1,5),step = 0.1),
+      sliderInput("tau_range_slider", "Bounds for parameter tau:",
+                  min = 0, max = 10, value = c(0,1),step = 0.01),
+      helpText("Termination conditions:"),
+      numericInput("maxTime_ode_input", "Max. optim. time [s]", 15, min = 0, max = 120),
+      numericInput("maxEval_ode_input", "Max. obj.func evaluation", 5000, min = 0, max = 1e5)
+      ),
     
     mainPanel(
       helpText("PlotFit - CNORode"),
+      plotOutput("PlotPrepModel_CNORode",width = "100%"),
       plotOutput("PlotFit_CNORode",width = "100%")
       # hr(),
       # helpText("PlotModel - CNORode"),
